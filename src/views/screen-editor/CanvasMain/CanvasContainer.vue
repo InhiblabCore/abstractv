5243a0d212fc3d02bca27b69d915d6e4e252f170
yangjie <1013588891@qq.com>
2022-03-18 17:17:48 +0800

    style: 去掉分号

--- src/views/screen-editor/CanvasMain/CanvasContainer.vue
+++ src/views/screen-editor/CanvasMain/CanvasContainer.vue
@@ -36,43 +36,43 @@
 </template>
 
 <script lang="ts" setup>
-  import useCanvasScale from '@/hooks/useCanvasScale';
-  import { useEditorComStore } from '@/store/modules/editorCom';
-  import { CSSProperties } from 'vue';
-  import { useEventEmitter, useHover } from 'vue3-hooks-plus';
-  import { Direction, getCursors, handleMove, handleRotate } from './utils';
-  const editorComStore = useEditorComStore();
-  const event = useEventEmitter({ global: true });
+  import useCanvasScale from '@/hooks/useCanvasScale'
+  import { useEditorComStore } from '@/store/modules/editorCom'
+  import { CSSProperties } from 'vue'
+  import { useEventEmitter, useHover } from 'vue3-hooks-plus'
+  import { Direction, getCursors, handleMove, handleRotate } from './utils'
+  const editorComStore = useEditorComStore()
+  const event = useEventEmitter({ global: true })
 
-  const instance = getCurrentInstance();
+  const instance = getCurrentInstance()
 
   const props = defineProps<{
     component: {
       attr: {
-        w: number;
-        h: number;
-        x: number;
-        y: number;
-        deg: 0;
-      };
-      selected: boolean;
-      locked: boolean;
-      hided: boolean;
-      hovered: boolean;
-      componentId: string;
-    };
-  }>();
+        w: number
+        h: number
+        x: number
+        y: number
+        deg: 0
+      }
+      selected: boolean
+      locked: boolean
+      hided: boolean
+      hovered: boolean
+      componentId: string
+    }
+  }>()
 
   event.useSubscription('select', (data: any) => {
-    const id = data?.params?.[0]?.componentId;
-    if (id === props.component.componentId) editorComStore.setComponentSelect(props.component);
-    else editorComStore.cancelComponentSelect(props.component);
-  });
+    const id = data?.params?.[0]?.componentId
+    if (id === props.component.componentId) editorComStore.setComponentSelect(props.component)
+    else editorComStore.cancelComponentSelect(props.component)
+  })
 
-  const { canvasScale: scale } = useCanvasScale();
+  const { canvasScale: scale } = useCanvasScale()
 
-  const containScaleRef = ref();
-  const isHover = useHover(containScaleRef);
+  const containScaleRef = ref()
+  const isHover = useHover(containScaleRef)
 
   const CanvasContainerStyle = computed(() => ({
     top: 0,
@@ -80,17 +80,17 @@
     width: `${props.component.attr.w}px`,
     height: `${props.component.attr.h}px`,
     transform: `translate(${props.component.attr.x}px, ${props.component.attr.y}px)`,
-  }));
+  }))
 
-  const cursor = computed(() => getCursors(props.component.attr.deg));
+  const cursor = computed(() => getCursors(props.component.attr.deg))
   const points = computed<{
     [k in Direction]: {
-      name: string;
-      style: Partial<CSSProperties>;
-      rotateStyle?: Partial<CSSProperties>;
-    };
+      name: string
+      style: Partial<CSSProperties>
+      rotateStyle?: Partial<CSSProperties>
+    }
   }>(() => {
-    const transform = `scale(${1 / scale.value}, ${1 / scale.value})`;
+    const transform = `scale(${1 / scale.value}, ${1 / scale.value})`
     return {
       t: {
         name: 'top',
@@ -128,38 +128,38 @@
         style: { cursor: cursor.value.lt },
         rotateStyle: { 'transform-origin': '75% 75%', transform },
       },
-    };
-  });
+    }
+  })
   const canvasContainerClass = computed(() => ({
     selected: props.component.selected,
     hided: props.component.hided,
     locked: props.component.locked,
-  }));
+  }))
   const hideStyle = computed(() => ({
     display: props.component.hided ? 'none' : 'block',
-  }));
+  }))
   const handlerClass = computed(() => ({
     hided: !props.component.selected || props.component.locked,
-  }));
+  }))
 
   const handlerStyle = computed(() => ({
     cursor: 'move',
     transform: `rotate(${props.component.attr.deg}deg)`,
-  }));
+  }))
   const wrapperStyle = computed(() => ({
     width: `${props.component.attr.w}px`,
     height: `${props.component.attr.h}px`,
-  }));
+  }))
 
   const selectCom = () => {
     if (props.component.selected) {
-      return;
+      return
     }
-    event.emit('select', { componentId: props.component.componentId });
-  };
+    event.emit('select', { componentId: props.component.componentId })
+  }
   const onRotate = (ev: MouseEvent) => {
-    handleRotate(ev, instance?.vnode.el as HTMLElement, props.component);
-  };
+    handleRotate(ev, instance?.vnode.el as HTMLElement, props.component)
+  }
 
   const onMove = (e: MouseEvent) => {
     handleMove({
@@ -167,15 +167,15 @@
       mouseStartEvent: e,
       grid: 8,
       scale: scale.value,
-    });
-    selectCom();
-  };
+    })
+    selectCom()
+  }
   const onZoom = (ev: MouseEvent, dir: Direction) => {
-    selectCom();
-    console.log(ev, dir);
+    selectCom()
+    console.log(ev, dir)
 
     //   handleZoom(ev, dir, props.com, scale.value);
-  };
+  }
 </script>
 <style lang="scss" scoped>
   @import './style.scss';
